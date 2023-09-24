c8be43a73286e5029d26288013fdfd60d7ce7161
AIZAWA Hina <hina@fetus.jp>
2023-09-25 05:55:49 +0900

    Switch random byte generator to Random\Randomizer
      - Native support requires PHP 8.2.
    
      - Use polyfill in PHP 8.0 and 8.1.
    
      - Functions specifying detailed random sources are deprecated.
        They will be removed in the next major version.
        They should not be used since they are in an internal class to begin with.

--- src/internal/Random.php
+++ src/internal/Random.php
@@ -10,17 +10,10 @@ declare(strict_types=1);
 
 namespace jp3cki\uuid\internal;
 
+use Random\Engine\Secure as SecureRandomEngine;
+use Random\Randomizer;
 use Throwable;
 use jp3cki\uuid\Exception;
-
-use function file_exists;
-use function file_get_contents;
-use function function_exists;
-use function is_readable;
-use function is_string;
-use function openssl_random_pseudo_bytes;
-use function random_bytes;
-use function strlen;
 
 final class Random
 {
@@ -29,58 +22,38 @@ final class Random
      */
     public static function bytes(int $length): string
     {
-        return self::byPHP7Random($length)
-            ?? self::byUnixRandom($length)
-            ?? self::byOpenSSLRandom($length)
-            ?? throw new Exception('No random source');
+        $generator = new Randomizer(new SecureRandomEngine());
+        try {
+            return $generator->getBytes($length);
+        } catch (Throwable $e) {
+            throw new Exception('No random source', 0, $e);
+        }
     }
 
     /**
+     * @deprecated
      * @param int<1, max> $length
      */
     public static function byPHP7Random(int $length): ?string
     {
-        if (function_exists('random_bytes')) {
-            try {
-                $tmp = random_bytes($length);
-                if (strlen($tmp) === $length) {
-                    return $tmp;
-                }
-            } catch (Throwable $e) {
-            }
-        }
-
-        return null; // @codeCoverageIgnore
+        return self::bytes($length);
     }
 
     /**
+     * @deprecated
      * @param int<1, max> $length
      */
     public static function byUnixRandom(int $length): ?string
     {
-        if (@file_exists('/dev/urandom') && @is_readable('/dev/urandom')) {
-            $tmp = @file_get_contents('/dev/urandom', false, null, 0, $length);
-            if (is_string($tmp) && strlen($tmp) === $length) {
-                return $tmp;
-            }
-        }
-
-        return null; // @codeCoverageIgnore
+        return self::bytes($length);
     }
 
     /**
+     * @deprecated
      * @param int<1, max> $length
      */
     public static function byOpenSSLRandom(int $length): ?string
     {
-        if (function_exists('openssl_random_pseudo_bytes')) {
-            $strong = null;
-            $r = @openssl_random_pseudo_bytes($length, $strong);
-            if (is_string($r) && strlen($r) === $length && $strong) {
-                return $r;
-            }
-        }
-
-        return null; // @codeCoverageIgnore
+        return self::bytes($length);
     }
 }
